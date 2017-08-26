45889fd05b3eb05159c2cd04b734f3e01dcbcee6
RosaEllis <31099300+RosaEllis@users.noreply.github.com>
2017-08-26 14:30:19 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,7 @@
 import scraperwiki
 html = scraperwiki.scrape('http://inmo.ie/6022')
 print "Click on the ...more link to see the whole page"
-print html
+# print html
 
 # -----------------------------------------------------------------------------
 # 1. Parse the raw HTML to get the interesting bits - the part inside <td> tags.
@@ -18,14 +18,19 @@ print html
 # -----------------------------------------------------------------------------
 # below is importing a library called lxml.html  
 import lxml.html
-# below makes a variable called root that is using the function called fromstring (from the lxml.html library)... 
-# it uses the html variable created above and it into an lxml object
- 
-root = lxml.html.fromstring(html) 
-# so 'root' now holds the results of the fromstring function being on the html we've scraped
-# 
 
-tds = root.cssselect('td') # get all the <td> tags
+# below makes a variable called root that is using the function called fromstring (from the lxml.html library)... 
+# it uses the html variable created above and it into an lxml object, which means we can use lxml functions on it 
+ 
+root = lxml.html.fromstring(html)
+# so 'root' now holds the results of the fromstring function being on the html we've scraped 
+
+# below is using the function cssselece with the root variable just created (which is the html we've just scraped turned... 
+# ...into an lxml object, and it gives it the variable name tds. 
+# this will grab all the td tags from the html, a td tag is for a cell in an html table 
+tds = root.cssselect('td') 
+
+# below is a for loop, that says for all the td(s) in tds, print the full html tag and print just the text 
 for td in tds:
     print lxml.html.tostring(td) # the full HTML tag
     print td.text                # just the text inside the HTML tag
